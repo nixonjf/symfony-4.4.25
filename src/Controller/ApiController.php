fb82c9f77cfbac71a157a52e1391f2831488f913
Nixon <you@examm>
2021-07-01 20:06:28 +0530

    initial commit

---
+++ src/Controller/ApiController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use App\Entity\User;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class ApiController extends AbstractController {
+
+    public function __construct(UserPasswordEncoderInterface $passwordEncoder) {
+        $this->passwordEncoder = $passwordEncoder;
+    }
+
+    public function login(Request $request): Response {
+
+        $email = 'lkl@dd.ccc';
+        $password = 'lkl@dd.ccc';
+
+
+        $entityManager = $this->getDoctrine()->getManager();
+
+        $user = $entityManager->getRepository(User::class)->findOneBy(['email' => $email]);
+
+
+        if (!$user) {
+            return JsonResponse::create(['status' => 'User not found'], 404, [], true);
+        }
+
+        if (!$this->passwordEncoder->isPasswordValid($user, $password)) {
+            return JsonResponse::create(['status' => 'Incorrect password'], 404, [], true);
+        }
+
+
+        return JsonResponse::create(['data' => ['apiKey' => $user->getApiKey()], 'status' => 'Success'], 200, [], true);
+    }
+
+}
---
+++ src/Controller/indexController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class indexController  extends AbstractController{
+
+    public function index(): Response { 
+        $user = $this->getUser();
+        
+        return new Response(
+                '<html><body>Welcome  '.$user->getUsername().'! <br> <a href="/logout">logout</a></body></html>'
+        );
+    }
+     
+
+}
